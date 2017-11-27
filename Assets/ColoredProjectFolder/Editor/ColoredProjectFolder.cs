aa6128cbbfbeffa0fe319bdad01db80b1bacb0cb
Ha.Minh <minhhh@minhhuyha.info>
2017-11-27 14:05:19 +0700

    v0.0.1

---
+++ Assets/ColoredProjectFolder/Editor/ColoredProjectFolder.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[InitializeOnLoad]
+public class ColoredProjectFolder
+{
+    static Texture2D iconEditorFolder;
+    static Texture2D iconPluginsFolder;
+    static Texture2D iconResourcesFolder;
+    static Texture2D iconPrefabsFolder;
+    static Texture2D iconScriptsFolder;
+
+    static ColoredProjectFolder ()
+    {
+        EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemOnGUI;
+        iconEditorFolder = (Texture2D)AssetDatabase.LoadAssetAtPath ("Assets/ColoredProjectFolder/Editor/icon_editor.png", typeof(Texture2D));
+        iconPluginsFolder = (Texture2D)AssetDatabase.LoadAssetAtPath ("Assets/ColoredProjectFolder/Editor/icon_plugins.png", typeof(Texture2D));
+        iconResourcesFolder = (Texture2D)AssetDatabase.LoadAssetAtPath ("Assets/ColoredProjectFolder/Editor/icon_resources.png", typeof(Texture2D));
+        iconPrefabsFolder = (Texture2D)AssetDatabase.LoadAssetAtPath ("Assets/ColoredProjectFolder/Editor/icon_prefabs.png", typeof(Texture2D));
+        iconScriptsFolder = (Texture2D)AssetDatabase.LoadAssetAtPath ("Assets/ColoredProjectFolder/Editor/icon_scripts.png", typeof(Texture2D));
+    }
+
+    private static void OnProjectWindowItemOnGUI (string guid, Rect selectionRect)
+    {
+        string path = AssetDatabase.GUIDToAssetPath (guid);
+        if (!AssetDatabase.IsValidFolder (path)) {
+            return;
+        }
+
+        selectionRect.width = selectionRect.height + 2;
+
+        var folderName = Path.GetFileNameWithoutExtension (path);
+        if (folderName == "Editor") {
+            GUI.DrawTexture (selectionRect, iconEditorFolder);    
+        } else if (folderName == "Plugins") {
+            GUI.DrawTexture (selectionRect, iconPluginsFolder);
+        } else if (folderName == "Resources") {
+            GUI.DrawTexture (selectionRect, iconResourcesFolder);
+        } else if (folderName == "Prefabs") {
+            GUI.DrawTexture (selectionRect, iconPrefabsFolder);
+        } else if (folderName == "Scripts") {
+            GUI.DrawTexture (selectionRect, iconScriptsFolder);
+        }
+
+
+    }
+    
+
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+# ColoredProjectFolders
+
+> Give colors to folders in Project View
+
+ColoredProjectFolders is a simple plugin that color some special folders in the Project View so that you can find them more easily. It is inspired by [Rainbow Folders](https://www.assetstore.unity3d.com/en/#!/content/50668).
+
+There's only 1 script `Assets/ColoredProjectFolder/Editor/ColoredProjectFolder.cs`. You can modify it to add more hard-coded folder names. You can add or modify folder icons in the same folders. This could be made into a customizable setting, but I feel the usage is the same as just modifying the script directly.
+
+## Setup
+
+1. Clone this repo and use it as a base for your new project
+2. Install `npm` and `yarn`
+3. Update `package.json` to include `ColoredProjectFolders` like so
+
+    ```
+        "ColoredProjectFolders": "ssh://github.com/minhhh/ColoredProjectFolders.git#v0.0.1"
+    ```
+4. Run `npm run unity` and wait for it to install all necessary packages
+
+## Changelog
+
+**0.0.1**
+
+* Initial commit
+
+<br/>
